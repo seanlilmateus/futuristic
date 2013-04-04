cb5fd0b74bf9f259b812c540ad592fa41308f3a1
seanlilmateus <seanlilmateus@yahoo.de>
2013-04-04 20:53:54 +0200

    remove old gem file

--- lib/futuristic/atomic/concurrent_update_error.rb
+++ lib/futuristic/atomic/concurrent_update_error.rb
@@ -1,6 +1,8 @@
-class Atomic
-  class ConcurrentUpdateError < ThreadError
-    # frozen pre-allocated backtrace to speed ConcurrentUpdateError
-    CONC_UP_ERR_BACKTRACE = ['backtrace elided; set verbose to enable'].freeze
+module Dispatch
+  class Atomic
+    class ConcurrentUpdateError < ThreadError
+      # frozen pre-allocated backtrace to speed ConcurrentUpdateError
+      CONC_UP_ERR_BACKTRACE = ['backtrace elided; set verbose to enable'].freeze
+    end
   end
 end
--- lib/futuristic/version.rb
+++ lib/futuristic/version.rb
@@ -1,3 +1,3 @@
 module Futuristic
-  VERSION = "0.4.3"
+  VERSION = "0.5.0"
 end
